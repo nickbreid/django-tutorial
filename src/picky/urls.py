8285a509f26698de0ac99d1cb95f70274cfe5d8b
Nick Reid <reidnickb@gmail.com>
2017-12-29 14:42:27 -0500

    Illustrates dynamic querysets Adds restaurant model and updates restaurants to use class-based views Uses get_queryset method to return dynamic data based upon kwargs

--- src/picky/urls.py
+++ src/picky/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from restaurants.views import home, contact, about
+from restaurants.views import (
+    RestaurantListView
+    )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^contact/$', contact),
-    url(r'^about/$', about),
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
+    url(r'^contact/$', TemplateView.as_view(template_name="contact.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html")),
+    url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
 ]
